d82fa61b7f40d616abf05c6b412761809e09b9e3
Saad-A-Rahman <saad.a.rahman15@gmail.com>
2023-12-17 11:13:26 +0000

    This code returns your name.

---
+++ name_returner.py
@@ -0,0 +1,13 @@
+name1 = input("Write a name:  ")
+name2 = input("Write another name:  ")
+name3 = input("Write one more name:  ")
+print("You will see each name three times.")
+
+for course in range(3):
+    print(name1)
+
+for course in range(3):
+    print(name2)
+
+for course in range(3):
+    print(name3)
